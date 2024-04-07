dfba0a4df4707d048602e9e1d67e26107cdd0585
LwinMateo <lymateo-lopez@myseneca.ca>
2024-04-07 01:26:34 -0400

    more screens

--- App.js
+++ App.js
@@ -5,25 +5,34 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
+import 'react-native-gesture-handler';
+
 // import screens
+import LoginScreen from './screens/LoginScreen';
 import ListingScreen from './screens/ListingScreen';
 import BookingScreen from './screens/BookingScreen';
+
+
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
 
+function TabNavigator() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Listing" component={ListingScreen} />
+      <Tab.Screen name="Booking" component={BookingScreen} />
+    </Tab.Navigator>
+  );
+}
+
 export default function App() {
   return (
-    // <View style={styles.container}>
-    //   <Text>Open up App.js to start working on your app!</Text>
-    //   <StatusBar style="auto" />
-    // </View>
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Listing" component={ListingScreen} />
-        <Tab.Screen name="Booking" component={BookingScreen} />
-
-      </Tab.Navigator>
+      <Stack.Navigator screenOptions={{headerShown: false}}>
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Home" component={TabNavigator} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
